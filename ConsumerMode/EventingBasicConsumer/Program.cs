768dce688a037a79eebd8a4b420fa116e99fa125
Xing <x.xing.unsw@gmail.com>
2020-08-26 22:08:59 +1000

    ConsumerMode

---
+++ ConsumerMode/EventingBasicConsumer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace EventingBasicConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exchange = "consumerModeExchange";
+            var eventConsumerQueue = "eventConsumerQueue";
+
+            // Create factory
+            var factory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                UserName = "XingXing",
+                Password = "XingXing"
+            };
+
+            // Create connection
+            var connection = factory.CreateConnection();
+
+            // Create channel
+            var channel = connection.CreateModel();
+
+            // Declare exchange
+            channel.ExchangeDeclare(exchange, ExchangeType.Direct, true, false, null);
+
+            // Declare queue
+            channel.QueueDeclare(eventConsumerQueue, true, false, false, null);
+
+            // Bind queue
+            channel.QueueBind(eventConsumerQueue, exchange, "", null);
+
+            // Set Qos, narrowing the channel allow the consumer to consume the msg, the msg will not lose
+            channel.BasicQos(0, 1, false);
+
+            // Create consumer
+            var consumer = new RabbitMQ.Client.Events.EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Body);
+                Console.WriteLine(msg);
+                Thread.Sleep(1000 * 1);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(eventConsumerQueue, false, consumer);
+            Console.Read();
+        }
+    }
+}
---
+++ ConsumerMode/Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exchange = "consumerModeExchange"; 
+
+            // Create factory
+            var factory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                UserName = "XingXing",
+                Password = "XingXing"
+            };
+
+            // Create connection
+            var connection = factory.CreateConnection();
+
+            // Create channel
+            var channel = connection.CreateModel();
+
+            // Declare exchange
+            channel.ExchangeDeclare(exchange, ExchangeType.Direct, true, false, null);
+
+            // Publish Msg
+            for (int i = 0; i < 10000; i++)
+            {
+                var msg = Encoding.UTF8.GetBytes($"{i}: Hello");
+                channel.BasicPublish(exchange, "", null, msg);
+            }
+        }
+    }
+}
